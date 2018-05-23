425d3892efd689e61aa9728aad32bfadf24b8b17
Mitch Hansen <37028540+mitchahan@users.noreply.github.com>
2018-05-22 21:41:38 -0700

    Initial Code
    I does what it looks like it does, this iterates of the DOM and replaces any text related to our 45th President.

---
+++ myscript.js
@@ -0,0 +1,40 @@
+walk(document.body);
+
+function walk(node)
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+	switch ( node.nodeType )
+	{
+		case 1:
+		case 9:
+		case 11:
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3:
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode)
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bPresident Trump\b/g, "Fuhrer Oompa Loompa");
+	v = v.replace(/\bPresident Donald Trump\b/g, "Fuhrer Orange Oompa Loompa");
+	v = v.replace(/\bDonald Trump\b/g, "Orange Oompa Loompa");
+	v = v.replace(/\bDonald\b/g, "Orange");
+	v = v.replace(/\bTrump\b/g, "Oompa Loompa");
+	textNode.nodeValue = v;
+}
